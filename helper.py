d6b19abf894aa4e8d8d5b46fc199e0e497cddd65
MahmoudAdelAziz <mahmoudaziz501@gmail.com>
2022-12-20 05:18:06 +0200

    Revert "Trying out another approach"
    This reverts commit cb74d0e8e501a377edd070d39000f00c3510b75b.

--- helper.py
+++ helper.py
@@ -1,14 +1,11 @@
 # Some helper functions
-import re
-
-cursor = 0  # global cursor (points to the current token)
+index = 0  # global cursor (points to the current token)
 error_state = False
 
 
-def DeclareError(errorMessage):
+def DeclareError():
     global error_state
     error_state = True
-    print(errorMessage)
 
 
 def CheckSyntax():
@@ -20,57 +17,10 @@ def CheckSyntax():
 
 
 def Match(expectedToken):
-    global cursor, token
+    global index, token
     # note: token is the current tokens[index] the parser's global cursor is pointing at
     if(token == expectedToken):
         # consume and advance
-        cursor += 1
-    else:
-        DeclareError()
-
-
-def Factor():
-    # factor --> (exp) | number | identifier
-    global cursor, token
-    if(re.search("^\\d+$", token)):
-        # it is a constant number
-        # this is an ultimate base case
-        cursor += 1
-    elif(token.isalpha()):
-        # It is an identifier
-        # this is another ultimate base case
-        cursor += 1
-    # TO BE CONTINUED
-
-
-def ComparisonOp():
-    # comparison-op --> < | =
-    global cursor, token
-    if(token == '<'):
-        Match('<')
-    elif(token == '>'):
-        Match('>')
-    else:
-        DeclareError()
-
-
-def AddOp():
-    # add-op --> + | -
-    global cursor, token
-    if(token == '+'):
-        Match('+')
-    elif(token == '-'):
-        Match('-')
-    else:
-        DeclareError()
-
-
-def MulOp():
-    # add-op --> *
-    global cursor, token
-    if(token == '*'):
-        Match('*')
-    elif(token == '/'):
-        Match('/')
+        index += 1
     else:
         DeclareError()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Imports
-from helper import * # some helper functions and variables
+import helper # some helper functions
 
 # Taking input
 print("Please Enter the name of the tokens list file (with extension, like tokens.txt): ")
@@ -14,14 +14,7 @@ tokens = [line.split(r',') for line in Lines]
 # cleaning up the tokens list (of spaces and new line chars, etc.)
 tokens = [[item[0].strip(), item[1].strip()] for item in tokens]
 
-# Define the current token
-number_of_tokens = len(tokens)
-token = tokens[cursor][0]
-# When (cursor == number_of_tokens & error_state = False) this means the given tokens
-# correspond to a syntactically valid TINY Language code.
+
 
 # Debugging
 print(tokens)
-print("--"*40)
-print(f"The current parser index is {cursor}")
-print(f"The current token is {token}")
